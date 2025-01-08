9d09d81cf0f4cece066e0b9104f6380b049b716c
Sebastian Zadomen <sebastian.zadomen.7e8@itb.cat>
2025-01-08 20:12:48 +0100

    prueba

---
+++ Program/MainProgram/Program.cs
@@ -0,0 +1,51 @@
+﻿using MyLibrary;
+using System;
+
+namespace MainProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*string result;
+            int userseg = Convert.ToInt32(Console.ReadLine());
+            int seg = userseg;
+            int cont = 0;
+            int min = 0;
+
+  
+                if (seg >= 60)
+                {
+                    cont++;
+                    seg = seg - 60;
+                    min = seg;
+
+                }
+            if (seg <= 60)
+            {
+                seg = seg / 100;
+                seg = seg * 10;
+                   
+            }
+                 
+           Console.WriteLine($"{cont} hora, {min} minutos,{seg}segundos");*/
+
+            int bas = int.Parse(Console.ReadLine());
+            int altura = int.Parse(Console.ReadLine());
+
+            int result;
+            if (bas > 0 && altura > 0)
+            {
+                result = bas * altura;
+                result = result / 2;
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Algunos de sus numeros es negativo");
+            }
+        }
+    }
+    }
+
+
---
+++ Program/MyLibrary/Math.cs
@@ -0,0 +1,20 @@
+﻿namespace MyLibrary
+{
+    public class Math
+    {
+        public static string ConvertTime (int seg) 
+            {
+            string result;
+            seg = seg / 60;
+            int cont= 0;
+
+            if (seg == 60)
+            { 
+                cont = 1;
+                seg = seg - 60;
+           
+            }
+            return result=$"{cont} hora, {seg} segundos,";
+            }
+    }
+}
